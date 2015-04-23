83d53a6772208cfe9a67afb92202637a01dacb3f
psykopotatis <psykopotatis@gmail.com>
2015-04-23 17:48:30 +0200

    Remove contact

--- project_name/urls.py
+++ project_name/urls.py
@@ -5,7 +5,6 @@ urlpatterns = patterns('',
     url(r'^$', 'app_name.views.index', name='index'),
     url(r'^sv$', 'app_name.views.index_sv', name='index_sv'),
     url(r'^sv/om-sidan$', 'app_name.views.about', name='about'),
-    url(r'^sv/kontakt', 'app_name.views.contact', name='contact'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
